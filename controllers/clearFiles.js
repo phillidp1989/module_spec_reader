8f403b73e77097ce1c2c260c2b0c7400f463104d
Dan Phillips <d.p.phillips@bham.ac.uk>
2022-04-20 23:08:32 +0100

    Clear files controller function to remove all file

---
+++ controllers/clearFiles.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const directory = path.join(__dirname, '..', 'client', 'public', 'uploads');
+
+// Remove all files from the uploads folder
+const clearFiles = (req, res, next) => {
+    let response = [];
+    const files = fs.readdirSync(directory);
+    for (const file of files) {
+        fs.unlink(path.join(directory, file), err => {
+          if (err) throw err;
+            response.push(file);
+        });
+    }
+    return res.status(200).send(response);
+}
+
+module.exports = clearFiles;
+
