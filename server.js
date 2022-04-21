1af7cf4fc6d9a2c712fbb647f4d49b06f77d4122
Dan Phillips <d.p.phillips@bham.ac.uk>
2022-04-21 15:02:49 +0100

    Required fs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const apiRoutes = require('./routes/apiRoutes');
+const fs = require('fs');
 
 
 const app = express();
