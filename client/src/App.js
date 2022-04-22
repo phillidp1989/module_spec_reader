dfa49a0360ffa0604263d24733bea8876ee350c3
Dan Phillips <d.p.phillips@bham.ac.uk>
2022-04-22 15:58:42 +0100

    Added card

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,13 @@ import FileUpload from './components/FileUpload';
 
 function App() {
   return (
-    <div className="container mt-4">     
-      <h4 className="display-4 text-center mb-4">Module Spec Upload</h4>
+    <div className="container mt-5" style={{width: "100%"}}>         
+      <h4 className="display-4 text-center mb-4 fw-bold">Module Spec Upload</h4>
+    <div className='col-sm-12 col-md-10 d-flex justify-content-center m-auto'>
+    <div className="card p-5 shadow rounded" style={{width: "100%"}}>
       <FileUpload />      
+      </div>
+    </div>
     </div>
   );
 }
